4a7cf08b01b76fbe75e331d40f1336d72ff2fa41
Sophie Tjhi <sophie.tjhi@gmail.com>
2025-08-02 14:36:28 +0800

    Initial Commit

---
+++ Bobby_da_stickman/Bobby_da_stickmanApp.swift
@@ -0,0 +1,17 @@
+//
+//  Bobby_da_stickmanApp.swift
+//  Bobby_da_stickman
+//
+//  Created by Sophie Tjhi on 2/8/25.
+//
+
+import SwiftUI
+
+@main
+struct Bobby_da_stickmanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
